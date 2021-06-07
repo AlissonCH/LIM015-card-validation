899eec48e1983b87c23a6de523c2f6d9b16e6667
AlissonCh <rocio.cruz@unmsm.edu.pe>
2021-06-07 17:13:31 -0500

    AliCH - commit 1
    Proyecto Card Validation funcional hasta test 100% con isValid y maskify. Requerimientos mínimos del proyecto

--- src/index.js
+++ src/index.js
@@ -1,9 +1,58 @@
-function saveCardNumber() {
-    const saveNumber = parseInt(document.getElementById('cardNumber').nodeValue);
-    alert('mostrar'+saveNumber);
+import validator from './validator.js';
+
+const payButton = document.getElementById('payButton');
+const inputNumber = document.getElementById('inputNumber');
+const errorText = document.getElementById('errorMessage');
+const validationIcon = document.getElementById('validationIcon');
+
+//icono "x" limpia el Nº de tarjeta
+function numberClean () {
+  const clean = inputNumber.value = '';
+  errorText.style.display = 'none';
+  document.getElementById('showMaskify').style='none';
+  return clean;
+} 
+validationIcon.addEventListener('click',numberClean);
+
+/*Captura el Nº de tarjeta como string, validación de espacios y que el N° de tarjeta admita sólo números, 
+en caso sea correcto: asigna number como parámetro de validator();
+en caso no: aparece un mensaje de error*/
+
+function sendNumber () {
+  const cardNumber = inputNumber.value;
+  if(/^\d{13,18}$/.test(cardNumber)) {
+    errorText.style.display = 'none';
+    return alert(`Esta tarjeta ${validator.isValid(cardNumber)}`);
+  } else {
+    errorText.style.display = 'block';
+  }
 }
+//botón que ejecuta la función sendNumber al dar click
+payButton.addEventListener('click',sendNumber);
 
-//import validator from './validator.js';
+const runMaskify = () => {
+  const cardNumber = inputNumber.value;
+  if(/^\d{13,18}$/.test(cardNumber)) {
+    errorText.style.display = 'none';
+    document.getElementById('showMaskify').style.display= 'block';
+    document.getElementById('showMaskify').innerHTML = validator.maskify(cardNumber);
+  }  else {
+    errorText.style.display = 'block';
+  }
+}
+document.getElementById('inputNumber').addEventListener('blur', runMaskify)
 
 
-//console.log(validator);
+
+
+/*function keyupInput () {
+  let numberLength = cardNumber.value.length;
+  console.log (numberLength);
+
+  if(numberLength<13) {
+    return console.log('El número de tarjeta debe contener por lo menos 13 dígitos');
+  
+  } else(13<numberLength<18)
+    return console.log('ok');
+  }
+cardNumber.addEventListener('keyup', keyupInput);*/
