b0f7e1138398f734c777a9454a02745ecd62e229
AlissonCh <rocio.cruz@unmsm.edu.pe>
2021-06-11 12:33:40 -0500

    agregué una tarjeta en el css dónde se refleja digito por digito el Nº de tarjeta y la fecha

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import validator from './validator.js';
-
 const payButton = document.getElementById('payButton');
 const inputNumber = document.getElementById('inputNumber');
 const errorText = document.getElementById('errorMessage');
 const validationIcon = document.getElementById('validationIcon');
+const imageCardNumber = document.getElementById('imageCardNumber');
+const imageDate = document.getElementById('imageDate');
+const imgLogo = document.getElementById('logo');
+
 //Muestra ventana PopUp
 const overLay = document.getElementById('overLay');
 const popUp = document.getElementById('popUp');
 const closePopUp = document.getElementById('btnCerrarPopUp');
+const buyText=document.getElementById('buy');
 function overLayFx(){
   overLay.classList.add('active');
   popUp.classList.add('active');
 }
+
 //cierra venta PopUp
 closePopUp.addEventListener('click',function(){
   overLay.classList.remove('active')
   popUp.classList.remove('active');
 });
-//función que muestra el validador y el maskify
+
+//función que muestra el validador y el maskify en el popUp
 function validateAndMaskify() {
   const cardNumber = inputNumber.value;
   if(/^\d{13,18}$/.test(cardNumber)) {
@@ -27,9 +33,11 @@ function validateAndMaskify() {
     if(validator.isValid(cardNumber)){
       document.getElementById('resultCardValidation').style.color = 'rgb(0,128,0)';
       document.getElementById('resultCardValidation').innerHTML= `ES VÁLIDA`;
+      buyText.innerText = 'Su compra se está procesando...';
     }else{
       document.getElementById('resultCardValidation').style.color = 'rgb(255,99,71)';
       document.getElementById('resultCardValidation').innerHTML= `NO ES VÁLIDA`;
+      buyText.innerText = 'Por favor vuelva a intentarlo';
     }
   } else {
     inputNumber.style.borderColor = 'rgb(255,99,71)';
@@ -41,49 +49,42 @@ payButton.addEventListener('click',validateAndMaskify);
 //mensaje de error
 const showErrorText = () => {
   const cardNumber = inputNumber.value;
-  if(/^\d{13,18}$/.test(cardNumber)) {
-    inputNumber.style.borderColor = '#3f8585'
-    errorText.style.display = 'none';
-  }  else {
+  if((/^\d{13,18}$/.test(cardNumber))===false) {
     inputNumber.style.borderColor = 'rgb(255,99,71)';
     errorText.style.display = 'block';
-  }
-}
-document.getElementById('inputNumber').addEventListener('blur', showErrorText);
-
-//icono "x" limpia el Nº de tarjeta
-function numberClean () {
-  const errorTextDisplay = errorText.style.display='block';
-  const showMaskifyDisplay= document.getElementById('showMaskify').style='block';
-  if(errorTextDisplay===true){
-    errorText.style.display = 'none';
-  } else if(showMaskifyDisplay===true){
-    return document.getElementById('showMaskify').style='none';
-  } else { 
-    const clean = inputNumber.value = '';
+    imgLogo.src = 'img/logos/something.png';
+  }  else {
     inputNumber.style.borderColor = '#3f8585'
     errorText.style.display = 'none';
-    return clean;
-  } 
+  }
+}
+inputNumber.addEventListener('blur', showErrorText);
+
+//icono "x" limpia el Nº de tarjeta y el mensaje de error
+function inputClean () {
+  errorText.style.display='none';
+  inputNumber.value = '';
+  inputNumber.style.borderColor = '#3f8585';
+  imageCardNumber.textContent = '';
+  imgLogo.src = 'img/logos/something.png';
 } 
-validationIcon.addEventListener('click',numberClean);
+validationIcon.addEventListener('click',inputClean);
 
 //Que muestre el logo cambiado cuando se escriba la primera letra
 function showLogo() {
-const imgLogo = document.getElementById('logo');
 const cardNumber = inputNumber.value;
 imgLogo.src = 'img/logos/' + (validator.changeLogo(cardNumber) || 'something')+ '.png';// cambiar la imagen del logo en el DOM
 }
 inputNumber.addEventListener('change', showLogo);
 
-//Mostrar el input de cardNumber en la tarjeta de imagen
-const imageCardNumber = document.getElementById('imageCardNumber');
-function showCardNumberImage(e) {
+//Mostrar el input Nº de tarjeta en cardImage
+function showImageCardNumber(e) {
   imageCardNumber.textContent = e.target.value;
 }
-inputNumber.addEventListener('keyup',showCardNumberImage);
-
-
-
-
-
+inputNumber.addEventListener('keyup',showImageCardNumber);
+//Mostrar la fecha en cardImage
+function showImageDate(e) {
+  imageDate.textContent = e.target.value;
+}
+const inputDate = document.getElementById('date');
+inputDate.addEventListener('keyup',showImageDate);
